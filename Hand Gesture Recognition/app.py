b333f6bccc49262e6fa8721c5417580afd8ce93d
DeboDam <Debojeetdam.business@gmail.com>
2024-07-22 23:39:14 -0600

    Moving Files around for Hand Gesture Recognition

--- Hand Gesture Recognition/app.py
+++ Hand Gesture Recognition/app.py
@@ -9,7 +9,6 @@ from collections import Counter, deque
 import cv2 as cv
 import mediapipe as mp
 import numpy as np
-
 from model import KeyPointClassifier, PointHistoryClassifier
 from utils import CvFpsCalc
 
@@ -59,7 +58,7 @@ def main():
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(
         static_image_mode=use_static_image_mode,
-        max_num_hands=2,
+        max_num_hands=1,
         min_detection_confidence=min_detection_confidence,
         min_tracking_confidence=min_tracking_confidence,
     )
